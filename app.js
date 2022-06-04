a7cdf42d294fd3554ec25cf37636645637fa7f30
Evan Bay <neopythe@protonmail.ch>
2022-06-04 11:31:40 -0600

    adjust session config

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true,
+    // secure: true,
     sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   },
